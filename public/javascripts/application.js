882caffcd0472138498de092971771dc92fcc5f0
Lucjan Suski <lucjansuski@gmail.com>
2011-09-24 14:45:57 +0000

    clean

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,3 @@
     $(function() {
         $("#login-trigger").leanModal({ top : 200, overlay : 0.4 });		
     });
-  // registration
-    $(function() {
-        $("#registration-trigger").leanModal({ top : 200, overlay : 0.4 });		
-    });
