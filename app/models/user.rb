5212fa86bf8f4144aa3c9eb80b7880a793ebeac7
Lucjan Suski <lucjansuski@gmail.com>
2011-10-12 14:37:08 +0000

    fixed avatar cropping and cleaned js

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,6 +82,7 @@ class User
 
   # Common validations
   validates_presence_of :login
+  validates_length_of :password, :minimum => 6
 
   with_options :if => :user_validatable? do |validatable|
     validatable.validates_length_of :phone_number, :is => 9, :allow_blank => true
